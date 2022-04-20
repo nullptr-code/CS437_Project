7a99b7da119ee008942983bd9401aa34b6088493
ShayaanJ <23100208@lums.edu.pk>
2022-04-21 02:48:03 +0500

    saved labels and uploaded to drive

--- generate_labels.py
+++ generate_labels.py
@@ -4,10 +4,10 @@ import skimage.io as io
 import random
 import os
 import cv2
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 ### For visualizing the outputs ###
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from tqdm import tqdm
 
 JSON_PATH = "dataset/annotation.json"
 IMAGE_PATH = "dataset/images/"
@@ -44,29 +44,53 @@ print("Number of images containing all the  classes:", len(imgIds))
 #-----------------------------------------------------------------------
 # load and display a random image
 #-----------------------------------------------------------------------
-img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
-I = io.imread(IMAGE_PATH  + img['file_name'])/255.0
 
-annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIDs, iscrowd=None)
-anns = coco.loadAnns(annIds)
-# print("annotation ids -> ", anns)
-# coco.showAnns(anns)
+LABEL_PATH = "dataset/labels/"
 
-print("Number of annotations in the image:", len(anns))
+for i in tqdm(imgIds):
+    img = coco.loadImgs(i)[0]
+    I = io.imread(IMAGE_PATH + img['file_name'])
+    annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIDs, iscrowd=None)
+    anns = coco.loadAnns(annIds)
+    mask = np.zeros((img['height'],img['width']))
+    for i in range(len(anns)):
+        className = getClassName(anns[i]['category_id'], cats)
+        pixel_value = filterClasses.index(className)+1
+        mask = np.maximum(coco.annToMask(anns[i])*pixel_value, mask)
+    mask = mask.astype(np.int16) * 255
+    # fig = plt.figure(figsize=(8, 8))
+    # fig.add_subplot(2, 1, 1)
+    # plt.imshow(I)
+    # fig.add_subplot(2, 1, 2)
+    # plt.imshow(mask)
+    # plt.show()
+    cv2.imwrite(LABEL_PATH + img['file_name'], mask)
+    # break
 
 
-#-----------------------------------------------------------------------
-# -------------------------------------adding masks
-#-----------------------------------------------------------------------
-mask = np.zeros((img['height'],img['width']))
-for i in range(len(anns)):
-    className = getClassName(anns[i]['category_id'], cats)
-    pixel_value = filterClasses.index(className)+1
-    mask = np.maximum(coco.annToMask(anns[i])*pixel_value, mask)
+# img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
+# I = io.imread(IMAGE_PATH  + img['file_name'])/255.0
 
-fig = plt.figure(figsize=(8, 8))
-fig.add_subplot(2, 1, 1)
-plt.imshow(I)
-fig.add_subplot(2, 1, 2)
-plt.imshow(mask)
-plt.show()
+# annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIDs, iscrowd=None)
+# anns = coco.loadAnns(annIds)
+# # print("annotation ids -> ", anns)
+# # coco.showAnns(anns)
+
+# print("Number of annotations in the image:", len(anns))
+
+
+# #-----------------------------------------------------------------------
+# # -------------------------------------adding masks
+# #-----------------------------------------------------------------------
+# mask = np.zeros((img['height'],img['width']))
+# for i in range(len(anns)):
+#     className = getClassName(anns[i]['category_id'], cats)
+#     pixel_value = filterClasses.index(className)+1
+#     mask = np.maximum(coco.annToMask(anns[i])*pixel_value, mask)
+
+# fig = plt.figure(figsize=(8, 8))
+# fig.add_subplot(2, 1, 1)
+# plt.imshow(I)
+# fig.add_subplot(2, 1, 2)
+# plt.imshow(mask)
+# plt.show()
